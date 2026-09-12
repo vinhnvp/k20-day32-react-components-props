e23237a5f72c3c8c831bb5add10092410b6cae88
vinhnvp <vinhnvphv@fullstack.edu.vn>
2026-09-13 01:10:13 +0700

    init: create homework 1 - day 32 - react component props

---
+++ homework-01/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-xl font-bold underline">
+      Hello, Vite + React!
+    </h1>
+  )
+}
+
+export default App
